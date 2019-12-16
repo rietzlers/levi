f3f009fe13acd7586e6032690416223dada33a34
Stephan Rietzler <stephan.rietzler@gmx.de>
2019-12-16 17:59:39 +0100

    remove hover info

--- inst/levi/R/spectrum_vc.R
+++ inst/levi/R/spectrum_vc.R
@@ -4,9 +4,13 @@
 spectrumUI <- function(id) {
   ns <- NS(id)
 
-  tagList(plotlyOutput(ns("spectrum"), height = 250),
-          uiOutput(ns("spectrum_info")),
-          verbatimTextOutput(ns("spectrum_click_info")))
+  tagList(
+    plotlyOutput(ns("spectrum_plot"), height = 250),
+    uiOutput(ns("spectrum_info")),
+    selectInput(ns("type"), label = "type", selected = "raw", choices = c("raw", "log")),
+    verbatimTextOutput(ns("select_info")),
+    verbatimTextOutput(ns("click_info"))
+  )
 }
 
 
@@ -18,42 +22,52 @@ spectrum_ctrl <- function(input, output, session, data, signal, frame_rate){
     estimate_signal_spectrum(data(), signal(), frame_rate())
   })
 
-  output$spectrum <-
+  output$spectrum_plot <-
     renderPlotly({
-      est_spec_plot <-
-        est_spec() %>%
-        ggplot(aes(x = freq, y = spec)) +
-        geom_line() +
-        labs(x = "Frequency [Hz]",
-             y = "log(periodogram)")
-
-      est_spec_plot %>%
-        ggplotly(source = ns("spectrum")) %>%
-        event_register("plotly_click")
-
-
+      spec_plot(est_spec(), type = input$type) %>%
+        ggplotly(source = ns("spectrum_plotly")) %>%
+        layout(dragmode = "select") %>%
+        event_register("plotly_brushed") %>%
+        event_register("plotly_selected")
     })
 
-  output$spectrum_info <-
-    renderUI({
-      max_freq <-
-        est_spec() %>%
-        filter(near(spec, max(spec), tol = 0.5)) %>%
-        summarize(max_freq = mean(freq))
-      h5(str_glue("Maximum Freq.: {round(max_freq$max_freq, 2)} Hz"))
-    })
-
-  output$spectrum_click_info <- renderPrint({
-    click_info <- event_data("plotly_click", source = ns("spectrum"))
-    if (is.null(click_info)) "Click events appear here (double-click to clear)" else click_info
+  output$spectrum_info <- renderUI({
+    max_freq <-
+      est_spec() %>%
+      filter(near(spec, max(spec), tol = 0.5)) %>%
+      summarize(max_freq = mean(freq))
+    h5(str_glue("Maximum Freq.: {round(max_freq$max_freq, 2)} Hz"))
   })
 
+  output$select_info <- renderPrint({
+    event_data("plotly_brushed", source = ns("spectrum_plotly"))
+    })
+
+  output$click_info <- renderPrint({
+    event_data("plotly_click", source = ns("spectrum_plotly"))
+  })
+
+}
+
+spec_plot <- function(est_spec, type = "raw"){
+  ylab = "raw"
+  if( type == "log"){
+    ylab = "log"
+    est_spec <- est_spec %>% mutate(spec = log(spec))
+  }
+
+    est_spec %>%
+    ggplot(aes(x = freq, y = spec)) +
+    geom_line() +
+    labs(x = "Frequency [Hz]",
+         y = ylab)
 }
 
 estimate_signal_spectrum <- function(data, signal, frame_rate) {
   est_spec <-
     spectrum(
-      ts(data  %>% pull(!!signal), frequency = frame_rate))
+      ts(data  %>% pull(!!signal), frequency = frame_rate),
+      plot = FALSE)
 
-  tibble(freq = est_spec$freq, spec = log(est_spec$spec))
+  tibble(freq = est_spec$freq, spec = est_spec$spec)
 }
