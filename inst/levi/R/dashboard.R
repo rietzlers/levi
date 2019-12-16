7ac6dce1803222018592a63bb2b42e4c6bb50154
Stephan Rietzler <stephan.rietzler@gmx.de>
2019-12-16 13:49:12 +0100

    exp_timing as reactive tibble

--- inst/levi/R/dashboard.R
+++ inst/levi/R/dashboard.R
@@ -4,39 +4,57 @@ dashboardUI <- function(id) {
   tagList(
     box(width = 4,
         fileInput(ns("file"), label = "select tevi-data (.dat-file)", accept = c(".dat")),
-        parametersUI(ns("params")),
-        verbatimTextOutput(ns("tevi_data_table"))),
+        parametersUI(ns("params"))),
     box(width = 8,
         plotOutput(ns("plot_center_xy"), height = 200),
         signalUI(ns("temp")),
         signalUI(ns("heating")),
         fluidRow(
           column(2, selectInput(ns("hp"), label = NULL, choices = c("one" = "hp1", "two" = "hp2", "three" = "hp3"))),
-          column(2, actionButton(ns("save"), label = "save", icon = icon("archive"))),
-          column(8, verbatimTextOutput(ns("exp_timing")) )))
+          column(8, verbatimTextOutput(ns("exp_timing")))))
     )
 }
 
 dashboard <- function(input, output, session){
 
   tevi_data <- reactive({import_tevi_data(session, input$file)})
-  exp_timing <- reactiveValues()
 
-  observeEvent({input$save; liquid_cooling()},{
-      exp_timing[["liquid_cooling"]] <-  liquid_cooling()
-      exp_timing[[input$hp]] <- hp_range()})
+  gen_exp_timing <- function(){
+    exp_timing <-
+      reactiveValues(
+        lc = c(NA_real_, NA_real_),
+        hp1 = c(NA_real_, NA_real_),
+        hp2 = c(NA_real_, NA_real_),
+        hp3 = c(NA_real_, NA_real_))
 
-  output$tevi_data_table <- renderPrint({tevi_data()})
+    observe({
+      exp_timing$lc <-  liquid_cooling()
+      exp_timing[[isolate(input$hp)]] <- hp_range()
+      })
+    # reset exp_timing with new data
+    observeEvent(input$file, {
+      for(name in names(exp_timing)){exp_timing[[name]] <- c(NA_real_, NA_real_)}})
+
+    reactive(as_tibble(reactiveValuesToList(exp_timing)) %>% select(lc, hp1, hp2, hp3))
+  }
+
+  exp_timing <- gen_exp_timing()
+
 
   output$plot_center_xy <-
-    renderPlot({center_coords(tevi_data(), as_tibble(reactiveValuesToList(exp_timing)))})
+    renderPlot({center_coords(tevi_data(), exp_timing())})
 
-  liquid_cooling <-  callModule(signal, "temp", tevi_data)
-  hp_range <- callModule(signal, "heating", tevi_data)
-  c(frame_rate) %<-% callModule(parameters, "params", tevi_data)
+  liquid_cooling <-
+    callModule(signal, "temp", tevi_data, "pyro_temp")
+
+  hp_range <-
+    callModule(signal, "heating", tevi_data, "htr_i")
+
+  c(frame_rate) %<-%
+    callModule(parameters, "params", tevi_data)
 
   output$exp_timing <-
-    renderPrint({as_tibble(reactiveValuesToList(exp_timing))})
+    renderPrint({exp_timing()})
 
   list(
     tevi_data,
@@ -46,16 +64,12 @@ dashboard <- function(input, output, session){
 
 center_coords <-
   function(data, exp_timing){
-    p <-
     data %>%
     ggplot(aes(x = time)) +
       geom_line(aes(y = center_x)) +
-      geom_line(aes(y = center_y), color = "red")
-
-
-    if(c("liquid_cooling") %in% names(exp_timing)){
-      p <- p + geom_vline(data = exp_timing, aes(xintercept = liquid_cooling), linetype = "dashed")
-    }
-    p
+      geom_line(aes(y = center_y), color = "red") +
+      geom_vline(data = exp_timing, aes(xintercept = lc), linetype = "dashed") +
+      geom_vline(data = exp_timing  %>% pivot_longer(cols = starts_with("hp"), values_to = "hp", values_drop_na = TRUE),
+                 aes(xintercept = hp), linetype = "dashed", color = "red")
   }
 
