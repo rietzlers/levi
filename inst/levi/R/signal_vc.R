307ea4da8ba4078e8426534b7971be8bbfba20ac
Stephan Rietzler <stephan.rietzler@gmx.de>
2019-12-17 17:30:42 +0100

    return selected signal;

--- inst/levi/R/signal_vc.R
+++ inst/levi/R/signal_vc.R
@@ -1,35 +1,32 @@
 # signal_vc.R #
 
 signalUI <- function(id, height = 200){
-
   ns <- NS(id)
 
   tagList(
     selectInput(ns("selected_signal"), label = "select signal", choices = NULL),
     plotOutput(
       ns("signal"), height = height,
-      brush = brushOpts(id = ns("brush"), fill = "#ccc", direction = "x", resetOnNew = TRUE)
-      )
-  )
+      brush = brushOpts(id = ns("brush"), fill = "#ccc", direction = "x", resetOnNew = TRUE))
+    )
 }
-
 
 signal <- function(input, output, session, data, variable = NULL){
 
-  observeEvent(
-    data(),
-    {
-      updateSelectInput(session, "selected_signal", choices = names(data()))
-      if(!is.null(variable)){
-        updateSelectInput(session, "selected_signal", selected = variable)
-      }
-    }
-    )
+  observeEvent(data(),
+      updateSelectInput(session, "selected_signal", choices = names(data()), selected = variable))
 
   output$signal <-
     renderPlot({gen_signal_plot(data(), input$selected_signal)})
 
-  return(reactive({get_brush_range(input$brush)}))
+  list(
+    signal =
+      reactive({
+        validate(need(input$selected_signal, "choose signal"))
+        rlang::sym(input$selected_signal)}),
+    brush =
+      reactive(input$brush)
+    )
 }
 
 gen_signal_plot <-
