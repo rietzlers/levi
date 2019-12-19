b47b6e8a59ff62244acf1bf39d9d58d24b4bad86
Stephan Rietzler <stephan.rietzler@gmx.de>
2019-12-19 11:36:35 +0100

    delete r-scripts imported from levitator

--- R/scripts/importRawTeviData.R
+++
@@ -1,9 +0,0 @@
-# import TeVi data files
-# Author: stephan
-###############################################################################
-
-# to do !
-# format of .dat-files has to be considered, e.g. transformed
-
-tevi <- read.table("./data/CuNiSi_PF2014B/CuNiSi_PF2014B_Day4_Pb14_rad.dat",header = TRUE)
-tevi$t <- tevi$Seconds - tevi[1,"Seconds"];
