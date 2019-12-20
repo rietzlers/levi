c8fd9263bde6686f040628424a562ff110ab54d0
Stephan Rietzler <stephan.rietzler@gmx.de>
2019-12-20 16:07:47 +0100

    test regular_ts function

---
+++ tests/testthat/test-periodogram.R
@@ -0,0 +1,40 @@
+context("fft and periodogram estimators")
+
+library(tidyverse)
+library(testthat)
+library(magrittr)
+
+testthat::test_that("non-regular ts are detected", {
+
+  sr <- 400
+  ts <- tibble(
+    t_c = seq(3, 8, by = 1/sr),
+    t_na = replace(t_c, sample(length(t_c), size = 20), NA_real_),
+    sig_c = rnorm(length(t_c)),
+    sig_na = replace(t_c, sample(length(t_c), size = 10), NA_real_)
+  )
+
+  expect_warning(regular_ts(ts %>% transmute(t = t_na), sr = 400),
+                 regexp = "NA values in t")
+  #expect_false(regular_ts(ts %>% transmute(t = t_na), sr = 400))
+
+  expect_warning(regular_ts(tibble(t = c(1,2,4)), sr = 400),
+                 regexp = "t_i are not equidistant or inconsistent with samplerate")
+
+  expect_warning(regular_ts(ts %>% transmute(t = t_c), sr = 401),
+                 regexp = "t_i are not equidistant or inconsistent with samplerate")
+
+  expect_warning(regular_ts(ts %>% transmute(t = t_c, signal = sig_na),
+                            sr = 400, signal = "signal"),
+                 regexp = "NA values in signal")
+
+  expect_true(regular_ts(ts %>% transmute(t = t_c, sig_c),
+                         sr = sr, signal = "sig_c"))
+})
+
+
+test_that("fft runs", {
+  sr <- 400
+  data <- tibble(t = seq(0, 2, by = 1/sr), s = cos(2*pi*30*t))
+
+})
