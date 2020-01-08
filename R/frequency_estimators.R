17efa002929bdd88058caae842254e226a2968da
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-01-08 15:17:47 +0100

    überarbeitet

--- R/frequency_estimators.R
+++ R/frequency_estimators.R
@@ -19,34 +19,49 @@ periodogram <- function(data, sr, signal, type){
 #' @param data tibble with columns f and fc_amp
 #' @param sr sample-rate
 #' @param c0 vector with start-parameters
+#' @param signal character
 #'
-#' @return list(fit_params, fitted[[lf_amp]]) NULL if nls did not converge
+#' @return list(fit_params, fitted[[f, lf_amp]]) NULL if nls did not converge
 #' @export
-fit_lorentz <- function(data, sr = 400, c0 = c(A = 1000, f0 = 30, g = 0.1) ) {
+fit_lorentz <- function(data, signal, sr = 400, c0 = c(A = 1000, f0 = 30, g = 0.1) ) {
   # falls nicht konvergiert: ändere Startwerte und versuche erneut. ca. 20 mal.
-  tryCatch({
+  fft_data <-
+    data %>% get_fc(signal, sr) %>%
+    mutate(
+      fc_squared = (fc_amp * 2)^2 # siehe schlittgen seite 56
+    )
+  c(f_dom, fc_amp) %<-% max_freq(fft_data, sample_rate = sr)
+  c0 <- c(A = (fc_amp * 2)^2, f0 = f_dom, g = 0.1)
+
+  print(c0)
+
+  # tryCatch({
     lfit <-
       nls(
-        fc_amp_squared ~ A / ((f ^ 2 - f0 ^ 2) ^ 2 + (g / 2) ^ 2 * f ^ 2),
-        data = data %>% filter(f < sr / 2) %>% mutate(fc_amp_squared = fc_amp^2),
+        fc_squared ~ A / ((f ^ 2 - f0 ^ 2) ^ 2 + (2*g) ^ 2 * f ^ 2),
+        data = fft_data %>% filter(between(f, f_dom - 10, f_dom + 10)),
         start =  c0,
-        trace = FALSE
+        trace = TRUE,
+        control = list(minFactor = 1/1024^2)
       )
-    fit_params = as_tibble(summary(lfit)$coeff) %>% janitor::clean_names()
-    fitted = data %>%
-      filter(f < sr / 2) %>%
-      mutate(lf_amp = sqrt(predict(lfit)))
-    list(
-      fit_params = fit_params,
-      fitted = fitted
-    )
-  },
-  warning = function(cond) {
-    return(list(fit_params = NULL, fitted = NULL))
-  },
-  error = function(cond) {
-    return(list(fit_params = NULL, fitted = NULL))
-  })
+    fit_params <-
+      as_tibble(summary(lfit)$coeff) %>% janitor::clean_names()
+    fitted <-
+      fft_data %>%
+      filter(between(f, f_dom - 10, f_dom + 10)) %>%
+      mutate(lf_amp = sqrt(predict(lfit)) / 2)
+
+    return(list(fit_params = fit_params,
+                fitted = fitted))
+  # },
+  # warning = function(cond) {
+  #   print("warning: nls of lorentz-fit")
+  #   return(list(fit_params = NULL, fitted = NULL))
+  # },
+  # error = function(cond) {
+  #   print("error: nls of lorentz-fit")
+  #   return(list(fit_params = NULL, fitted = NULL))
+  # })
 }
 
 
@@ -102,15 +117,16 @@ get_fc <- function(data, signal, sr){
 #' @return tibble with
 #' @export
 bp_filter <- function(sig_data, signal, bp, sr){
+  N <- length(sig_data$t)
   get_fc(sig_data, signal, sr) %>%
     mutate(
       t = sig_data$t,
       fc = if_else(
         between(f, bp[1], bp[2]) | between(f, (sr - bp[2]), (sr - bp[1])),
-        fc * length(t),
+        fc * N, # N = lenght(t): Normierung wieder rückgängig machen.
         0i
         ),
-      !!sym(signal) := Re(fft(fc, inverse = TRUE)) / length(t)
+      !!sym(signal) := Re(fft(fc, inverse = TRUE)) / N
     )
 }
 
@@ -144,7 +160,7 @@ regular_ts <- function(data, signal, sr) {
     warning("NA values in t")
     return(FALSE)
   }
-  if (any(!near(data %$% diff(t), 1 / sr))) {
+  if (any(!near(diff(data$t), 1 / sr))) {
     warning("t_i are not equidistant or inconsistent with samplerate")
     return(FALSE)
   }
