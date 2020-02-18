fe5e6f7544e45926f0f179570fdc399a126416db
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-02-18 14:38:20 +0100

    add sections/rename vars

--- inst/levi/R/sig_analysis.R
+++ inst/levi/R/sig_analysis.R
@@ -1,5 +1,6 @@
 # signal_plot.R #
 
+# view --------
 signalAnalysisUI <- function(id, width = 12) {
   ns <- NS(id)
   tagList(
@@ -11,14 +12,14 @@ signalAnalysisUI <- function(id, width = 12) {
   )
 }
 
-
+# controller --------
 signalAnalysis <- function(input, output, session, raw_tevi_data, frame_rate){
 
   ns <- session$ns
 
   data_selection <-
     reactive({
-      selected_data <-brushedPoints(raw_tevi_data(), signalbrush())
+      selected_data <-brushedPoints(raw_tevi_data(), signal_brush())
       validate(need(nrow(selected_data) > 0,
                     "select data by brushing (left-click and pull) over signal-plot"))
       selected_data
@@ -31,24 +32,24 @@ signalAnalysis <- function(input, output, session, raw_tevi_data, frame_rate){
     updateSelectInput(session, "signal_choice", choices = signal_names)
   })
 
-  c(signal, signalbrush) %<-% callModule(signal, "completeTimerange", raw_tevi_data, "radius_y")
 
-  # signal in range ------------------
-  output$signal_in_selected_range <-
-    renderPlot({ts_plot(data_selection(), signal())})
+  output$signal_in_selected_range <- renderPlot({ts_plot(data_selection(), signal_sym())})
 
-  # spectrum ---------
-  callModule(spectrum_ctrl, "spectrum", data_selection, signal, frame_rate)
-
-  # numerical summary --------------
   output$selected_signal_info <-
     renderUI({
       br <- get_brush_range(input$brush)
       h5(str_glue("Window center-point: {round(mean(br), 2)} s and window-width: {round(diff(br), 2)} s"))
     })
+
+  # submodules ----------
+  c(signal_sym, signal_brush) %<-% callModule(signal_ctrl, "completeTimerange", raw_tevi_data, "radius_y")
+
+  callModule(spectrum_ctrl, "spectrum", data_selection, signal_sym, frame_rate)
+
+ # return-values -----------
 }
 
-
+# helpers -----------------
 ts_plot <- function(ds, var){
   ds %>%
     ggplot(aes(x = time)) +
