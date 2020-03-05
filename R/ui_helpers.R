e43b7374065a84844a1091da51b7075cfbf7c2b1
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-03-05 14:07:02 +0100

    add default-values-option to get_brush_range

--- R/ui_helpers.R
+++ R/ui_helpers.R
@@ -2,14 +2,21 @@
 
 #' convert brush from plotOutput to numeric vector
 #'
+#'
 #' @param brush input-object
 #' @param failure_massage string: message to display in case there is no brush
+#' @param default_values range to return if no brush is selected
 #'
 #' @return numeric vector with the range of the brushed area
 #' @export
 get_brush_range <-
-  function(brush, failure_massage = "select range by brushing plot"){
-    validate(need(brush, failure_massage))
-    c(xmin, xmax, ...rest)  %<-% brush
-    c(xmin, xmax)
+  function(brush, failure_massage = "select range by brushing plot", default_values){
+    if(!missing(default_values) & !is.null(need(brush, failure_massage))){
+      return(default_values)
+    }
+    else{
+      validate(need(brush, failure_massage))
+      c(xmin, xmax, ...rest)  %<-% brush
+      c(xmin, xmax)
+    }
   }
