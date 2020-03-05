d40b1856a257ab5a1f4dc3ee28abf523a732ffd4
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-03-05 11:00:25 +0100

    major reorganization of import_tevi_data_vc
    setzen der experimentellen zeiten wird anders modelliert

--- inst/levi/R/import_tevi_data.R
+++ inst/levi/R/import_tevi_data.R
@@ -1,4 +1,4 @@
-# view ---------
+
 importTeviDataUI <- function(id) {
   ns <- NS(id)
   tagList(
@@ -7,57 +7,33 @@ importTeviDataUI <- function(id) {
         model_params_view(ns("params"))),
     box(width = 8,
         plotOutput(ns("plot_center_xy"), height = 200),
-        signalUI(ns("temp")),
-        signalUI(ns("heating")),
-        fluidRow(
-          column(6, selectInput(ns("hp"), label = NULL, choices = c("one" = "hp1", "two" = "hp2", "three" = "hp3"))),
-          column(6, verbatimTextOutput(ns("exp_timing")))))
-    )
+        plotOutput(ns("temp_plot"), brush = brushOpts(id = ns("temp_plot_brush"), fill = "#ccc", direction = "x", resetOnNew = FALSE)),
+        signalUI(ns("heating")))
+  )
 }
 
-# controller -----------
 importTeviData <- function(input, output, session) {
   # local data --------
   tevi_data <- reactive({
     import_tevi_data(session, input$file)
   })
-  gen_exp_timing <- function() {
-    exp_timing <-
-      reactiveValues(
-        lc = c(NA_real_, NA_real_),
-        hp1 = c(NA_real_, NA_real_),
-        hp2 = c(NA_real_, NA_real_),
-        hp3 = c(NA_real_, NA_real_)
-      )
-
-    observe({
-      exp_timing$lc <-  get_brush_range(lc_brush())
-      exp_timing[[isolate(input$hp)]] <- get_brush_range(hp_brush())
-    })
-    # reset exp_timing with new data
-    observeEvent(input$file, {
-      for (name in names(exp_timing)) {
-        exp_timing[[name]] <- c(NA_real_, NA_real_)
-      }
-    })
-
-    reactive(as_tibble(reactiveValuesToList(exp_timing)) %>% select(lc, hp1, hp2, hp3))
-  }
-  exp_timing <- gen_exp_timing()
-
 
   output$plot_center_xy <-
     renderPlot({
-      plot_center_coords(tevi_data(), exp_timing())
+      tevi_data() %>%
+        ggplot(aes(x = t)) +
+        geom_line(aes(y = center_x)) +
+        geom_line(aes(y = center_y), color = "red") +
+        labs(y = "center-coordinates")
     })
 
-  output$exp_timing <-
-    renderPrint({
-      exp_timing()
-    })
 
-  c(...skip, lc_brush) %<-%
-    callModule(signal_ctrl, "temp", tevi_data, "pyro_temp")
+  output$temp_plot <- renderPlot({
+    tevi_data() %>%
+      ggplot(aes(x = t)) +
+      geom_line(aes(y = pyro_temp)) +
+      geom_line(aes(y = smoothed_temp))
+  })
 
   c(...skip, hp_brush) %<-%
     callModule(signal_ctrl, "heating", tevi_data, "htr_i")
@@ -70,15 +46,5 @@ importTeviData <- function(input, output, session) {
        frame_rate)
 }
 
-# helpers ------------
-plot_center_coords <-
-  function(data, exp_timing){
-    data %>%
-    ggplot(aes(x = t)) +
-      geom_line(aes(y = center_x)) +
-      geom_line(aes(y = center_y), color = "red") +
-      geom_vline(data = exp_timing, aes(xintercept = lc), linetype = "dashed") +
-      geom_vline(data = exp_timing  %>% pivot_longer(cols = starts_with("hp"), values_to = "hp", values_drop_na = TRUE),
-                 aes(xintercept = hp), linetype = "dashed", color = "red")
-  }
+
 
