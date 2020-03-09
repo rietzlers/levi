5c37f4e482ad46107261978cdd6cc215cd040bdb
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-03-09 10:48:27 +0100

    rm model_params_vc.R
    die sample-und tevi-parameter werden nun in sample_specs_vc.R und import_tevi_data_vc.R
    ermittelt

--- inst/levi/R/load_dependencies.R
+++ inst/levi/R/load_dependencies.R
@@ -15,7 +15,6 @@ library(shinyBS)
 library(levi)
 
 # source code which needs to run once -------------------------------------
-source("R/model_params_vc.R")
 source("R/sample_specs_vc.R")
 source("R/sig_analysis.R")
 source("R/import_tevi_data.R")
