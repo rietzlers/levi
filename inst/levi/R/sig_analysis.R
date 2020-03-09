f6ac26f40ebed375d94508f7519a69eae0c135d8
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-03-09 15:30:34 +0100

    mv complete_sig_plot to sig_analysis.R

--- inst/levi/R/sig_analysis.R
+++ inst/levi/R/sig_analysis.R
@@ -2,16 +2,17 @@
 signalAnalysisUI <- function(id, width = 12) {
   ns <- NS(id)
   tagList(
-    box(width = width,signalUI(ns("completeTimerange"))),
-    box(width= width, plotOutput(ns("signal_in_selected_range"),  height = 150),
-        title = "Signal in selected range; raw and BP-filtered(blue)", collapsible = TRUE),
+    box(width = width, {
+      plotOutput(ns("complete_signal"), height = 200,
+                   brush = brushOpts(id = ns("signal_brush"), fill = "#ccc", direction = "x", resetOnNew = FALSE))}),
+    box(width= width, title = "Signal in selected range; raw and BP-filtered(blue)", collapsible = TRUE, {
+      plotOutput(ns("signal_in_selected_range"),  height = 150)}),
     box(width = width, spectrumUI(ns("spectrum_analysis"))),
     box(width = width, resultsUI(ns("results")))
   )
 }
 
-signalAnalysis <- function(input, output, session, tevi_model, sample_specs,
-                           dynamicSidebarItems, selected_tab){
+signalAnalysis <- function(input, output, session, tevi_model, sample_specs, dynamicSidebarItems, selected_tab){
   ns <- session$ns
   # data ----
   data_selection <- reactive({
@@ -25,8 +26,32 @@ signalAnalysis <- function(input, output, session, tevi_model, sample_specs,
     signal_names <- col_names[col_names != "t"]
     updateSelectInput(session, "signal_choice", choices = signal_names)
   })
+  observeEvent({selected_tab()},{
+                 if (selected_tab() != "importTeviData") {
+                   dynamicSidebarItems$signal_selection <-
+                     selectInput(session$ns("selected_signal"), label = NULL, choices = names(tevi_model()$tevi_data), selected = "radius_y")
+                 } else{
+                   dynamicSidebarItems$signal_selection <- NULL
+                 }
+               }) #update signal-selection
 
+  signal_brush <- reactive({input$signal_brush})
+  signal_name  = reactive({input$selected_signal})
   # UIs -------------
+  output$complete_signal <- renderPlot({
+    validate(need(input$selected_signal, label = "signal"))
+    sig_plot <-
+      tevi_model()$tevi_data %>%
+      ggplot(aes(x = t)) +
+      geom_line(aes_string(y = input$selected_signal))
+    if(is.null(need(tevi_model()$HPs, "heat-pulses"))){
+      sig_plot <- sig_plot + geom_vline(xintercept = tevi_model()$HPs %>% flatten_dbl(), linetype = "dashed", color = "red")
+    }
+    if(is.null(need(tevi_model()$exp_time_range, "exp_time_range"))){
+      sig_plot <- sig_plot + geom_vline(xintercept = tevi_model()$exp_time_range, linetype = "dashed", color = "blue")
+    }
+    sig_plot
+  })
   output$signal_in_selected_range <- renderPlot({
       ts_plot <- function(ds, signal_name, time_range, bp, frame_rate){
         ds %>%
@@ -46,9 +71,6 @@ signalAnalysis <- function(input, output, session, tevi_model, sample_specs,
         frame_rate = tevi_model()$frame_rate
         )} # call ts_plot with reactives
       })
-
-  c(signal_name, signal_brush) %<-%
-    callModule(signal_ctrl, "completeTimerange", tevi_model, "radius_y", dynamicSidebarItems, selected_tab)
 
   c(type, bp, dom_freq, f0, d, spans, taper, add_result) %<-%
     callModule(spectrum_ctrl, "spectrum_analysis", tevi_model, data_selection, signal_name)
