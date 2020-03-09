eb005827c41dcaa98cbbec0bc601a3d68fadd965
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-03-09 11:39:22 +0100

    place sample_specs-UI at bottom of sidebar

--- inst/levi/app.R
+++ inst/levi/app.R
@@ -1,12 +1,7 @@
-## app.R ##
-
 library(reactlog)
-
-# tell shiny to log all reactivity
 options(shiny.reactlog = TRUE)
 
 source(file.path("R/load_dependencies.R"), local = TRUE, encoding = "UTF-8")
-
 # view --------
 header <-
   dashboardHeader(
@@ -15,28 +10,21 @@ header <-
     dropdownMenu(type = "messages"),
     dropdownMenu(type = "tasks")
   )
-
-
 sidebar <-
   dashboardSidebar(sidebarMenu(id = "sidebarMenu",
     menuItem("Import Tevi Data",tabName = "importTeviData", icon = icon("dashboard")),
-    sample_specs_view("sample_specs"),
     menuItem("Signal Analysis", tabName = "signalAnalysis", icon = icon("bar-chart-o")),
-    uiOutput("signal_selection")
+    uiOutput("signal_selection"),
+    sample_specs_view("sample_specs")
   ))
-
-
 body <-
   dashboardBody(tabItems(
     tabItem(tabName = "importTeviData", importTeviDataUI("tdi")),
     tabItem(tabName = "signalAnalysis", signalAnalysisUI("sa"))
   ))
-
 ui <- dashboardPage(header, sidebar, body, title = "Alloy-EML-Analysis")
-
-# controller -----------------
-server <-
-  function(input, output, session) {
+# ctrl --------
+server <- function(input, output, session) {
 
     selectedSidebarMenu <- reactive(input$sidebarMenu) #returns the selected sidebar-tab
     dynamicSidebarItems <- reactiveValues(signal_selection = NULL)
@@ -45,7 +33,6 @@ server <-
       })
 
     sample_specs <- callModule(sample_specs_ctrl, "sample_specs")
-
     tevi_model <-  callModule(importTeviData, "tdi")
 
     callModule(signalAnalysis, "sa", tevi_model, sample_specs, dynamicSidebarItems, selectedSidebarMenu)
