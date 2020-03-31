791fae57fb443ca48867967446c71043f236db03
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-03-31 14:46:03 +0200

    modify report-notes

--- inst/levi/R/report_notes_vc.R
+++ inst/levi/R/report_notes_vc.R
@@ -2,19 +2,17 @@ report_notes_UI <- function(id){
   ns <- NS(id)
   tagList(
     textAreaInput(ns("notes"),
-                  label = HTML("Report Notes ( <a href='https://www.markdownguide.org/cheat-sheet'>Markdown Cheat-Sheet</a>)"), width = "100%", rows = 10,
+                  label = HTML("Report Notes (You can use <a href='https://www.markdownguide.org/cheat-sheet'>markdown</a> to format your notes.)"),
+                  width = "100%", rows = 10,
                   placeholder = "Notes on analysis."),
-    downloadButton(ns("gen_report"), "Download Notes as html-file"),
+    downloadButton(ns("gen_report"), "Download Notes as html-document"),
     bsTooltip(ns("gen_report"), "download report-notes as html-file", "right", options = list(container = "body"))
   )
 }
 
-report_notes_ctrl <- function(input, output, session, alloy, selected_tab, tasks, notifications){
-
+report_notes_ctrl <- function(input, output, session, sample_specs){
 
   output$gen_report <- downloadHandler(
-
-    # For PDF output, change this to "report.pdf"
     filename = "report_notes.html",
     content = function(file) {
       # Copy the report file to a temporary directory before processing it, in
@@ -25,13 +23,16 @@ report_notes_ctrl <- function(input, output, session, alloy, selected_tab, tasks
 
       # Set up parameters to pass to Rmd document
       params <- list(
+        alloy = "Alloy-Analysis",
         notes = input$notes
         )
 
       # Knit the document, passing in the `params` list, and eval it in a
       # child of the global environment (this isolates the code in the document
       # from the code in this app).
-      rmarkdown::render(tempReport, output_file = file,
+      rmarkdown::render(tempReport,
+                        output_format = "html_document",
+                        output_file = file,
                         params = params,
                         envir = new.env(parent = globalenv())
       )
