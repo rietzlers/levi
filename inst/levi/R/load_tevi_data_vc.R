ce96f22a92f573e66423060f36cb23fd76081352
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-03-31 17:21:24 +0200

    sample-spec-notification = warning

--- inst/levi/R/load_tevi_data_vc.R
+++ inst/levi/R/load_tevi_data_vc.R
@@ -62,8 +62,8 @@ load_tevi_data_ctrl <- function(input, output, session) {
       HTML(str_glue("Estimated frame-rate for data from file '{tevi_data_name()}' is: <b>{est_sample_freq} Hz </b>.")),
       duration = 5, type = "message")
     showNotification(
-      "Make sure you set the sample-specifications to match the loaded tevi-data",
-      duration = 5, type = "message")
+      "Tevi-data uploaded: Make sure to that the right sample-specs are selected!",
+      duration = 5, type = "warning")
     tevi_data_RV(tevi_data)
     session$resetBrush(session$ns("temp_plot_brush"))
   }) # load-tevi-data on input-file-change
