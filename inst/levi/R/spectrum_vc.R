fee612d426e2441d95bb9ec75749c46fd881c98e
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-04-10 15:17:04 +0200

    calc param_estimates with all methods; return as tidy-df

--- inst/levi/R/spectrum_vc.R
+++ inst/levi/R/spectrum_vc.R
@@ -17,8 +17,6 @@ spectrumUI <- function(id) {
       tabPanel("Controls",
                selectInput(ns("scale"), label = "Set scaling of spectrogram ordinate", selected = "log10", choices = c("raw", "log10")),
                bsTooltip(ns("scale"), "scaling of spectrogram-ordinate", "top"),
-               selectInput(ns("calc_method"), label = "Choose method to calculate Foruier-Coefficients", selected = "spectrum", choices = c("spectrum", "fft")),
-               bsTooltip(ns("calc_method"), "use FFT/spectrogram to calculate periodogram", "top"),
                textInput(ns("spans"), label = "span", value = "c(3,3)"),
                bsTooltip(ns("spans"), "specify daniell-smoother: NULL for no smoothing", "top", options = list(container = "body")),
                numericInput(ns("taper"), label = "taper", value = 0.1, step = .1, min = 0, max = 1),
@@ -64,12 +62,12 @@ spectrum_ctrl <- function(input, output, session, tevi_model, data_selection, si
   lfit_models <- reactive({
     lorentz_fit_to_fft <-
       levi::fit_lorentz(
-      dplyr::filter(spectrum_estimate(), type == "fft"),
+      dplyr::filter(spectrum_estimate(), calc_method == "fft"),
       bp = bp(), # lorentz-kurve wird IMMER an die BP-gefilterte Kurve angepasst!
       sr = tevi_model()$frame_rate)
     lorentz_fit_to_spectrum <-
       levi::fit_lorentz(
-        dplyr::filter(spectrum_estimate(), type == "spectrum"),
+        dplyr::filter(spectrum_estimate(), calc_method == "spectrum"),
         bp = bp(), # lorentz-kurve wird IMMER an die BP-gefilterte Kurve angepasst!
         sr = tevi_model()$frame_rate)
     fitted_models = list(
@@ -77,18 +75,55 @@ spectrum_ctrl <- function(input, output, session, tevi_model, data_selection, si
       to_spectrum_data = lorentz_fit_to_spectrum
     )
   })
-  dom_freq_estimates <- reactive({
+  parameter_estimates <- reactive({
+    sr <- tevi_model()$frame_rate
+
+    f_raw_estimates <-
+      spectrum_estimate() %>%
+      group_by(calc_method) %>%
+      get_dom_freq(sr) %>%
+      ungroup() %>%
+      transmute(
+        calc_method,
+        dom_freq_estimate = f
+      )
+
+    lfit_estimates <-
+     lfit_models() %>%
+      map_dfr(lorentz_parameters) %>%
+      set_names(c("fft_lorentz", "spectrum_lorentz")) %>%
+      mutate(parameter = c("A", "f0", "d")) %>%
+      pivot_longer(cols = contains("lorentz"), names_to = "calc_method")
+
+    f_lfit_estimates <-
+      lfit_estimates %>%
+      filter(parameter == "f0") %>%
+      transmute(
+        calc_method,
+        dom_freq_estimate = value
+      )
+
+    damping_estimates <-
+      lfit_estimates %>%
+      filter(parameter == "d") %>%
+      transmute(
+        calc_method,
+        damping_estimate = value,
+        spans = input$spans,
+        taper = as.numeric(input$taper)
+      )
+
+    f_raw_estimates %>%
+      dplyr::union(f_lfit_estimates) %>%
+      left_join(damping_estimates)
 
   })
 
-  damping_estimate <- reactive({
-
-  })
   dom_freq <- reactive({
     f_dom <- NULL
     c(f_dom, ...)  %<-%
       (spectrum_estimate() %>%
-         dplyr::filter(type == input$calc_method, f %>% between(bp()[1], bp()[2])) %>%
+         dplyr::filter(f %>% between(bp()[1], bp()[2])) %>%
          levi::get_dom_freq(sample_rate = tevi_model()$frame_rate) %>%
         round(2))
     validate(need(f_dom, "f_dom"))
@@ -109,9 +144,9 @@ spectrum_ctrl <- function(input, output, session, tevi_model, data_selection, si
     spectrum_estimate() %>%
       filter(f %>% between(0, tevi_model()$frame_rate/2)) %>%
       ggplot(aes(x = f)) +
-      geom_line(data = ~ dplyr::filter(.x, type == "spectrum"), aes(y = fc_amp)) +
-      geom_point(data = ~ dplyr::filter(.x, type == "spectrum"), aes(y = fc_amp), shape = "x", size = 0.8) +
-      geom_point(data = ~ dplyr::filter(.x, type == "fft", f > 0), aes(y = fc_amp), color = "blue", shape = "+", size = 1.5) +
+      geom_line(data = ~ dplyr::filter(.x, calc_method == "spectrum"), aes(y = fc_amp)) +
+      geom_point(data = ~ dplyr::filter(.x, calc_method == "spectrum"), aes(y = fc_amp), shape = "x", size = 0.8) +
+      geom_point(data = ~ dplyr::filter(.x, calc_method == "fft", f > 0), aes(y = fc_amp), color = "blue", shape = "+", size = 1.5) +
       scale_y_continuous(
         name = if_else(input$scale == "log10", "log10(Fourier-Coef-Amp)", "Fourier-Coef-Amp"),
         trans = if_else(input$scale == "log10", "log10", "identity")
@@ -120,6 +155,7 @@ spectrum_ctrl <- function(input, output, session, tevi_model, data_selection, si
     })
 
   output$bp_spectrum <- renderPlotly({
+    parameter_estimates()
       gen_spec_plot(
         spectrum_estimate(),
         lfit_models = lfit_models(),
@@ -131,13 +167,7 @@ spectrum_ctrl <- function(input, output, session, tevi_model, data_selection, si
 
   # return-values -----------
   list(
-    type = reactive(input$calc_method),
-    bp = bp,
-    dom_freq = dom_freq,
-    f0 = f0,
-    d = d,
-    spans = reactive(input$spans),
-    taper = reactive(input$taper)
+    parameter_estimates
   )
 
 }
