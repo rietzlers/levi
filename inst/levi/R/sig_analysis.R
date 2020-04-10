ca3cfa3bd6d60a7879f6823d400c8597be14fbef
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-04-10 16:54:52 +0200

    pass signal_name, window_range between modules

--- inst/levi/R/sig_analysis.R
+++ inst/levi/R/sig_analysis.R
@@ -12,9 +12,9 @@ signalAnalysis <- function(input, output, session, tevi_model, sample_specs, tas
 
 
   # module-calls --------
-  c(data_selection, signal_name) %<-% callModule(oscillogram_ctrl, "oscillogram", tevi_model)
-  parameter_estimates <- callModule(spectrum_ctrl, "spectrum_analysis",
-                                    tevi_model, data_selection, signal_name)
+  c(data_selection, signal_name, window_range) %<-% callModule(oscillogram_ctrl, "oscillogram", tevi_model)
+
+  parameter_estimates <- callModule(spectrum_ctrl, "spectrum_analysis", tevi_model, data_selection, signal_name, window_range)
 
   # callModule(results_ctrl, "results",
   #            tevi_model, sample_specs, data_selection, window_range, signal_name,
