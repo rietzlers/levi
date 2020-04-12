5437906720792dc749b7d16f5feea11866eeeae2
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-04-12 18:09:04 +0200

    add viscosoty tab; change icons in sidebar

--- inst/levi/app.R
+++ inst/levi/app.R
@@ -3,45 +3,72 @@
 
 source(file.path("R/load_dependencies.R"), local = TRUE, encoding = "UTF-8")
 
-ui <- function(request){
-  dashboardPage(title = "Alloy-EML-Analysis",
-  header =
-  dashboardHeader(
-    title = "levi",
-    dropdownMenuOutput("notifications"),
-    dropdownMenuOutput("tasks"),
-    dropdownMenuOutput("messages")
-  ),
-sidebar =
-  dashboardSidebar(sidebarMenu(id = "sidebarMenu_ID",
-    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-    menuItem("Signal-Analysis", icon = icon("signal"),
-             menuSubItem("Spectrum and Oscillogram", tabName = "spec_osc"),
-             menuSubItem("Spec+Dom-Freq", tabName = "spec_dom_freq"),
-             menuSubItem("Instantanous Frequency", tabName = "inst_freqs"),
-             menuSubItem("Smoothed Signal Envelope", tabName = "sig_envelope")),
-    menuItem("Surface-Tension", tabName = "surface_tension", icon = icon("microscope")),
-    menuItem("Simulate Data", tabName = "simulate_data_tab", icon = icon("microscope")),
-    uiOutput("resample_UI"),
-    bookmarkButton(
-      label = "Save current results",
-      title= HTML("Paste the URL into your browsers location-bar and then bookmark it with your browser. (Make sure to give the bookmark a mneomic name.) You can send this URL to a collaborator to share your current analysis-results. (Your collaborator needs access to the same shiny-server!)"),
-      width = "80%"
-    )
-  )),
-body =
-  dashboardBody(
-    tabItems(
-      tabItem(tabName = "dashboard", dashboard_UI("main_dashboard")),
-      tabItem(tabName = "simulate_data_tab", simulate_data_view("simulate_data")),
-      tabItem(tabName = "surface_tension", surface_tension_analysis_UI("st_analysis")),
-      tabItem(tabName = "spec_osc", seewave_view("spec_osc")),
-      tabItem(tabName = "spec_dom_freq", seewave_view("spec_dom_freq")),
-      tabItem(tabName = "inst_freqs", seewave_view("inst_freqs")),
-      tabItem(tabName = "sig_envelope", seewave_view("sig_envelope"))
+ui <- function(request) {
+  dashboardPage(
+    title = "Alloy-EML-Analysis",
+    header =
+      dashboardHeader(
+        title = "levi",
+        dropdownMenuOutput("notifications"),
+        dropdownMenuOutput("tasks"),
+        dropdownMenuOutput("messages")
+      ),
+    sidebar =
+      dashboardSidebar(
+        sidebarMenu(
+          id = "sidebarMenu_ID",
+          menuItem(
+            "Dashboard",
+            tabName = "dashboard",
+            icon = icon("dashboard")
+          ),
+          menuItem(
+            "Signal-Analysis",
+            icon = icon("signal"),
+            menuSubItem("Spectrum and Oscillogram", tabName = "spec_osc"),
+            menuSubItem("Spec+Dom-Freq", tabName = "spec_dom_freq"),
+            menuSubItem("Instantanous Frequency", tabName = "inst_freqs"),
+            menuSubItem("Smoothed Signal Envelope", tabName = "sig_envelope")
+          ),
+          menuItem(
+            "Surface-Tension",
+            tabName = "surface_tension_analysis_UI",
+            icon = icon("tint")
+          ),
+          menuItem(
+            "Viscosity",
+            tabName = "viscosity_analysis_UI",
+            icon = icon("vial")
+          ),
+          menuItem(
+            "Simulate Data",
+            tabName = "simulate_data_UI",
+            icon = icon("blind")
+          ),
+          uiOutput("resample_UI"),
+          bookmarkButton(
+            label = "Save current results",
+            title = HTML(
+              "Paste the URL into your browsers location-bar and then bookmark it with your browser. (Make sure to give the bookmark a mneomic name.) You can send this URL to a collaborator to share your current analysis-results. (Your collaborator needs access to the same shiny-server!)"
+            ),
+            width = "80%"
+          )
+        )
+      ),
+    body =
+      dashboardBody(
+        tabItems(
+          tabItem(tabName = "dashboard", dashboard_UI("main_dashboard")),
+          tabItem(tabName = "simulate_data_UI", simulate_data_view("simulate_data")),
+          tabItem(tabName = "surface_tension_analysis_UI", surface_tension_analysis_UI("st_analysis")),
+          tabItem(tabName = "viscosity_analysis_UI", "to be done"),
+          tabItem(tabName = "spec_osc", seewave_view("spec_osc")),
+          tabItem(tabName = "spec_dom_freq", seewave_view("spec_dom_freq")),
+          tabItem(tabName = "inst_freqs", seewave_view("inst_freqs")),
+          tabItem(tabName = "sig_envelope", seewave_view("sig_envelope"))
+        )
       )
-    )
-)
+  )
 }
 
 server <- function(input, output, session) {
@@ -64,14 +91,14 @@ server <- function(input, output, session) {
       notifications(notifications_list)
       } # sample_specs
     })# update notes
-  } # end notes, tasks and massages
+  }
 
   # dynamic sidebar content--------------
   {
     selected_sidebar_tab <- reactive(input$sidebarMenu_ID) #returns the selected sidebar-tab
     output$resample_UI <- renderUI(resample_UI())
     resample_UI <- reactiveVal()
-  } # dynamic sidebar content
+  }
 
   # global data ------
   model <- reactive({
@@ -81,6 +108,12 @@ server <- function(input, output, session) {
 
   # dashboard --------------
   c(tevi_model, sample_specs) %<-% callModule(dashboard_ctrl, "main_dashboard")
+
+  # data-simulation -----
+  c(sim_data_model, model_choice) %<-% callModule(simulate_data_ctrl, "simulate_data", resample_UI)
+  # surface-tension-analysis ----------
+  callModule(surface_tension_analysis_ctrl, "st_analysis", model, sample_specs, tasks, notifications)
+
   # signal-analysis ----------
   {
     callModule(seewave_ctrl, "spec_osc", model, selected_sidebar_tab)
@@ -88,11 +121,6 @@ server <- function(input, output, session) {
     callModule(seewave_ctrl, "inst_freqs", model, selected_sidebar_tab)
     callModule(seewave_ctrl, "sig_envelope", model, selected_sidebar_tab)
   }
-  # data-simulation -----
-  c(sim_data_model, model_choice) %<-% callModule(simulate_data_ctrl, "simulate_data", resample_UI)
-  # surface-tension-analysis ----------
-  callModule(surface_tension_analysis_ctrl, "st_analysis", model, sample_specs, tasks, notifications)
-
 
   }
 
