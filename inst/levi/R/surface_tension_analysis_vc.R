c87cb993af5af29ea36fb7bfe7fd9aec035bde8a
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-04-14 12:07:10 +0200

    data_selection is lokal to surface_tension_analysis_ctrl

--- inst/levi/R/surface_tension_analysis_vc.R
+++ inst/levi/R/surface_tension_analysis_vc.R
@@ -9,8 +9,14 @@ surface_tension_analysis_UI <- function(id) {
 }
 
 surface_tension_analysis_ctrl <- function(input, output, session, tevi_model, sample_specs, tasks, notifications){
+
+  data_selection <- reactive({
+    tevi_model()$tevi_data %>%
+      filter(t %>% between(window_range()[1], window_range()[2]))
+  })
+
   # oscillogram------
-  c(data_selection, signal_name, window_range) %<-% callModule(oscillogram_ctrl, "oscillogram", tevi_model)
+  c(signal_name, window_range) %<-% callModule(oscillogram_ctrl, "oscillogram", tevi_model)
   # periodogram----------
   parameter_estimates <- callModule(spectrum_ctrl, "spectrum_analysis", tevi_model, data_selection, signal_name, window_range)
   # results --------
