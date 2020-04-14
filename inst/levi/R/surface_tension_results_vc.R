d5c3a171d7c128575dc37f27c1403c00624936ba
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-04-14 16:24:39 +0200

    change st-result-plot and -data-table

--- inst/levi/R/surface_tension_results_vc.R
+++ inst/levi/R/surface_tension_results_vc.R
@@ -7,7 +7,6 @@ surface_tension_results_UI <- function(id){
           column(width = 2,
                  box(width = 12,
                  actionButton(ns("add_result"), label = "Save current values", icon = icon("save")),
-                 actionButton(ns("show_st_results"), label = "View Results-Table", icon = icon("database")),
                  selectInput(ns("st_xvar"), label = "Display: ", selected = "t", choices = c("time [s]" = "t", "Temp. [° C]" = "smoothed_temp")),
                  selectInput(ns("st_yvar"), label = NULL, selected = "f", choices = c("Freq. [Hz]" = "f","Surface-Tension [Nm]" = "st"))
                  )
@@ -15,10 +14,9 @@ surface_tension_results_UI <- function(id){
           column(width = 10,
                  plotlyOutput(ns("surface_tension_plot"), height = "350px"))
           )),
-      bsModal("st_results_view", "Surface-Tension Results", ns("show_st_results"),
-              DT::dataTableOutput(ns("spec_analsis_results_DT")),
-              size = "large"
-      )
+      box(width = 12, title = "Surface-Tension Results-Data", collapsible = TRUE, collapsed = TRUE,
+          DT::dataTableOutput(ns("spec_analsis_results_DT"))
+          )
     )
 }
 
@@ -83,11 +81,11 @@ surface_tension_results_ctrl <- function(input, output, session, tevi_model, sam
       y_unit <- "Nm"
     }
 
-    calc_method_trace <- function(p, cm, trace,  estimates = NULL, sl = TRUE){
-      if(missing(trace)){
-        trace = cm
-      }
-      p %>% filter(calc_method == cm) %>%
+    calc_method_trace <- function(p, cm, trace,  estimates = NULL, sl = TRUE, fit_lm = FALSE, farbe = "red"){
+      if(missing(trace)){trace = cm}
+      p <-
+        p %>%
+        filter(calc_method == cm) %>%
         add_trace(
           data = estimates,
           name = trace,
@@ -96,20 +94,45 @@ surface_tension_results_ctrl <- function(input, output, session, tevi_model, sam
           x = ~ get(x_var),
           y = ~ get(y_var),
           hovertemplate = paste("%{y:.2f}", y_unit),
-          showlegend = sl
+          showlegend = sl,
+          color = farbe
         )
+      if(fit_lm == TRUE){
+        complete_obs <-
+          plotly_data(p) %>%
+          filter(!is.na(dom_freq_estimate)) %>%
+          filter(calc_method == cm)
+        lm <- lm(get(y_var) ~ get(x_var), data = complete_obs)
+        c(y0, m) %<-% coef(lm)
+
+        lm_data <-
+          tibble(
+            x = complete_obs[[x_var]],
+            fitted_values = predict(lm)
+          )
+        p <-
+          p %>%
+          add_lines(
+            data = lm_data,
+            name = str_glue("{round(y0, 2)} + {round(m, 4)} x"),
+            x = ~x, y = ~fitted_values,
+            hoverinfo = "none",
+            color = farbe
+            )
+      }
+      p
     }
 
       spec_analysis_results() %>%
         plot_ly(source = "st_plot") %>%
-        add_fun(function(p){calc_method_trace(p, "fft")}) %>%
-        add_fun(function(p){calc_method_trace(p, "spectrum")}) %>%
-        add_fun(function(p){calc_method_trace(p, "fft_lorentz")}) %>%
-        add_fun(function(p){calc_method_trace(p, "spectrum_lorentz")}) %>%
-        add_fun(function(p){calc_method_trace(p, "fft", trace = " ", estimates = live_parameter_estimates(), sl = FALSE)}) %>%
-        add_fun(function(p){calc_method_trace(p, "spectrum",  trace = " ", estimates = live_parameter_estimates(), sl = FALSE)}) %>%
-        add_fun(function(p){calc_method_trace(p, "fft_lorentz",  trace = " ",  estimates = live_parameter_estimates(), sl = FALSE)}) %>%
-        add_fun(function(p){calc_method_trace(p, "spectrum_lorentz",  trace = " ",  estimates = live_parameter_estimates(), sl = FALSE)}) %>%
+        add_fun(function(p){calc_method_trace(p, "fft", fit_lm = TRUE, farbe = "blue")}) %>%
+        add_fun(function(p){calc_method_trace(p, "spectrum", fit_lm = TRUE, farbe = "green")}) %>%
+        add_fun(function(p){calc_method_trace(p, "fft_lorentz", fit_lm = TRUE, farbe ="yellow")}) %>%
+        add_fun(function(p){calc_method_trace(p, "spectrum_lorentz", fit_lm = TRUE, farbe = "orange")}) %>%
+        add_fun(function(p){calc_method_trace(p, "fft", trace = "current estimate", estimates = live_parameter_estimates(), sl = FALSE)}) %>%
+        add_fun(function(p){calc_method_trace(p, "spectrum",  trace = "current estimate", estimates = live_parameter_estimates(), sl = FALSE)}) %>%
+        add_fun(function(p){calc_method_trace(p, "fft_lorentz",  trace = "current estimate",  estimates = live_parameter_estimates(), sl = FALSE)}) %>%
+        add_fun(function(p){calc_method_trace(p, "spectrum_lorentz",  trace = "current estimate",  estimates = live_parameter_estimates(), sl = FALSE)}) %>%
         layout(
           legend = list(x = 0.8, y = 0.9),
           xaxis = list(title = x_axis_title,
@@ -122,21 +145,44 @@ surface_tension_results_ctrl <- function(input, output, session, tevi_model, sam
 
   # st-results-table ---------
   output$spec_analsis_results_DT <- DT::renderDataTable({
-      validate(need(spec_analysis_results(), label = "spec_analysis_results"))
-      spec_analysis_results() %>%
-        mutate(t = round(t, 2)) %>%
-        arrange(calc_method, t)
-    },
-    rownames = FALSE,
-    server = TRUE, filter = 'top',
-    extensions = c('Buttons', 'Responsive', 'FixedColumns'),
-    options = list(
-      dom = 'Bftlip',
-      buttons = c('csv', 'excel', 'pdf', I('colvis')),
-      pageLength = 5,
-      autoWidth = TRUE,
-      fixedColumns = list(leftColumns = 1)
-    ))
+    validate(need(spec_analysis_results(), label = "spec_analysis_results"))
+    spec_analysis_results() %>%
+      mutate(
+        t = round(t, 3),
+        dom_freq_estimate = round(dom_freq_estimate, 2),
+        damping_estimate = round(damping_estimate, 1),
+        win_length = round(win_end - win_start, 2),
+        win_start = round(win_start, 2),
+        win_end = round(win_end, 2),
+        temp = round(temp, 1),
+        st = round(st, 4)
+        ) %>%
+      select(
+        t, dom_freq_estimate, temp, st, damping_estimate, calc_method, everything()
+      ) %>%
+      mutate(
+        calc_method = factor(calc_method),
+        signal = factor(signal),
+        tevi_data_name = factor(tevi_data_name),
+        spans = factor(spans)
+      ) %>%
+      arrange(t, calc_method) %>%
+      datatable(
+        rownames = FALSE,
+        colnames = c("time [s]" = "t", "Estimate of dom. Freq. [Hz]" = "dom_freq_estimate",
+                     "Temp. [° C]" = "temp", "Surface-Tension [Nm]" ="st",
+                     "Damping-Estimate [1/s]" = "damping_estimate"),
+        filter = 'top',
+        extensions = c('Buttons', 'Responsive'),
+        options = list(
+          dom = 'Bftli',
+          buttons = c('csv', 'excel', 'pdf', I('colvis')),
+          paging = FALSE,
+          autoWidth = TRUE
+        )
+      )
+  },
+  server = TRUE)
 
   # return-values ----------
 
