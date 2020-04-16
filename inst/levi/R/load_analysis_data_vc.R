65c58798565c55a3f8556419ed990fcfe75bec6e
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-04-16 17:21:17 +0200

    restructure and rename load_analysis_data_vc

--- inst/levi/R/load_analysis_data_vc.R
+++ inst/levi/R/load_analysis_data_vc.R
@@ -14,82 +14,86 @@ load_tevi_data_UI <- function(id) {
         plotOutput(
           ns("heat_pulses_plot"), height = 100,
           brush = brushOpts(id = ns("heat_pulses_plot_brush"), fill = "#ccc", direction = "x", resetOnNew = FALSE)
-          ),
+        ),
         fluidRow(
           column(width = 2, actionButton(ns("save_hp"), label = "save hp Nr.:")),
           column(width = 3, numericInput(ns("hp_nr"), label = "", value = 1)),
           column(width = 5, textOutput(ns("hp_range")))
         )
-      )
     )
+  )
 }
 
 load_tevi_data_ctrl <- function(input, output, session) {
   ns <- session$ns
 
-  # local data --------
+  # tevi-data --------
   tevi_data_RV <- reactiveVal()
 
-  tevi_data <- reactive({
-    validate(need(tevi_data_RV(), message = "No Tevi-data available! Make sure you have uploaded a .csv-Tevi-file."))
-    tevi_data_RV()
+  observeEvent(input$file, {
+    # update raw-tevi-data
+    input$file$datapath %>% import_tevi_data() %>% tevi_data_RV()
+
+    showNotification(
+      "Tevi-data uploaded: Make sure to that the right sample-specs are selected!",
+      duration = 10, type = "warning"
+    )
+
+    # estimate sample/frame-rate from mean dt
+    c(est_sample_freq) %<-%
+      (tevi_data() %>% summarize(est_sample_freq = round(1 / mean(diff(t), na.rm = TRUE))))
+
+    updateNumericInput(
+      session, "frame_rate",
+      value = est_sample_freq
+    )
+
+    showNotification(
+      HTML(str_glue("Estimated frame-rate for data from file '{input$file$name}' is: <b>{est_sample_freq} Hz </b>.")),
+      duration = 10, type = "message")
   })
 
-  tevi_data_name <- reactive({
-    validate(need(input$file, message = "select file in file-upload-dialog"))
-    input$file$name
-  })
+  tevi_data <-
+    reactive({
+      validate(need(tevi_data_RV(), message = "No Tevi-data available! Make sure you have uploaded a .csv-Tevi-file."))
+      tevi_data_RV()
+    })
 
-  exp_time_range <- reactiveVal(NULL)
-  observeEvent(event_data("plotly_brushed", source = ns("temp_plot")),{
-    x_range <- event_data("plotly_brushed", source = ns("temp_plot"))$x
-    exp_time_range(x_range)
-  }, ignoreInit = TRUE)
+  observeEvent(
+    exp_time_range(),
+    tevi_data_RV(tevi_data_RV() %>% add_temperature(time_range = exp_time_range())),
+    ignoreInit = TRUE
+  )
 
-  analysis_data <- reactive({
-    validate(need(exp_time_range(), message = "can not define analysis-data as there is no exp_time_range"))
-    tevi_data() %>%
-      filter(t %>% between(exp_time_range()[1], exp_time_range()[2])) %>%
-      mutate(t = t - min(t, na.rm = TRUE))
-  })
+  # frame-rate ------
   frame_rate = reactive({
     validate(need(input$frame_rate, label = "frame_rate"))
     input$frame_rate
   })
-  hps <- reactiveValues()
 
+  # analysis-data/exp-time-range--------------
+
+
+  exp_time_range <- reactiveVal(NULL)
+  observeEvent(
+    event_data("plotly_brushed", source = ns("temp_plot")),
+    exp_time_range(event_data("plotly_brushed", source = ns("temp_plot"))$x),
+    ignoreInit = TRUE
+  )
+
+  analysis_data <- reactive({
+    validate(need(exp_time_range(), message = "No analysis-data as there is no exp_time_range defined! Define one in the load-tevi-data tab (Dashboard)"))
+    tevi_data() %>%
+      filter(t %>% between(exp_time_range()[1], exp_time_range()[2])) %>%
+      mutate(t = t - min(t, na.rm = TRUE))
+  })
+
+  # Heat-Puls ---------
+  hps <- reactiveValues()
   HPs = reactive({
     validate(need(hps, label = "heat-puls-data"))
     reactiveValuesToList(hps)
   })
-
-
-  # observe input-file-change ------
-  observeEvent(input$file, {
-    # update raw-tevi-data
-    tevi_data_RV(
-      import_tevi_data(
-        input$file$datapath))
-
-    showNotification(
-      "Tevi-data uploaded: Make sure to that the right sample-specs are selected!",
-      duration = 10, type = "warning")
-
-
-    # estimate sample/frame-rate from mean dt
-    c(est_sample_freq) %<-%
-      (tevi_data_RV() %>% summarize(est_sample_freq = round(1 / mean(diff(t), na.rm = TRUE))))
-
-    updateNumericInput(session, "frame_rate", value = est_sample_freq)
-
-    showNotification(
-      HTML(str_glue("Estimated frame-rate for data from file '{tevi_data_name()}' is: <b>{est_sample_freq} Hz </b>.")),
-      duration = 10, type = "message")
-  })
-
-  observeEvent(exp_time_range(),{
-      tevi_data_RV(tevi_data() %>% add_temperature(time_range = exp_time_range()))
-    }, ignoreInit = TRUE)# add temp-var to tevi_data
 
   observeEvent(input$save_hp,{
     hp_nr <- input$hp_nr
@@ -103,14 +107,15 @@ load_tevi_data_ctrl <- function(input, output, session) {
   onRestored(function(state){
     exp_time_range(state$values$exp_time_range)
   })
-  # output-ctrls -------------
+  # plot_center_xy  -------------
   output$plot_center_xy <- renderPlot({
+
     center_xy_plot <-
-        tevi_data() %>%
-        ggplot(aes(x = t)) +
-        geom_line(aes(y = center_x)) +
-        geom_line(aes(y = center_y), color = "red") +
-        labs(y = "center-coordinates")
+      tevi_data() %>%
+      ggplot(aes(x = t)) +
+      geom_line(aes(y = center_x)) +
+      geom_line(aes(y = center_y), color = "red") +
+      labs(y = "center-coordinates")
     if(is.null(need(hps, "hps"))){
       center_xy_plot <-
         center_xy_plot +
@@ -122,30 +127,34 @@ load_tevi_data_ctrl <- function(input, output, session) {
         geom_vline(xintercept = exp_time_range(), linetype = "dashed", color = "blue")
     }
     center_xy_plot
-    })
+  })
+
+  # temp_plot ------
   output$temp_plot <- renderPlotly({
+
     tevi_data() %>%
       plot_ly(x  = ~t, source = ns("temp_plot")) %>%
       add_lines(y = ~pyro_temp, name = "Measured", hovertemplate = "%{y:.0f} °C") %>%
       add_lines(
         name =
-        "Smoothed Temp.
+          "Smoothed Temp.
       (used for time - Temp conversion)",
         y = ~smoothed_temp,
         color = I("red"),
         hovertemplate = "%{y:.0f} °C"
-        ) %>%
+      ) %>%
       layout(
         legend = list(x = 0.8, y = 0.8),
         dragmode = "select",
         selectdirection = "h",
         xaxis = list(title = "time [s]"),
         yaxis = list(title = "Pyro-Temp. [°C]")
-        ) %>%
+      ) %>%
       event_register("plotly_brushed")
   })
-
+  # heat-puls-plot --------
   output$heat_pulses_plot <- renderPlot({
+
     tevi_data() %>%
       ggplot(aes(x = t, y = htr_i)) +
       geom_line()
@@ -153,23 +162,20 @@ load_tevi_data_ctrl <- function(input, output, session) {
   output$hp_range <- renderText({
     hps[[str_glue("hp{input$hp_nr}")]]
   })
+
   # return-values ----------
   reactive({
     validate(need(tevi_data_RV(),
                   message = "No Tevi-data available! Make sure you have uploaded a .csv-Tevi-file."))
     validate(need(analysis_data(),
                   message = "No analysis-time-range specified. Brush temp-plot in 'dashboard'-tab 'upload tevi-data' to set experimental time-range"))
-    browser()
     list(
       tevi_data = tevi_data(),
       analysis_data = analysis_data(),
-      tevi_data_name = tevi_data_name(),
+      tevi_data_name = input$file$name,
       exp_time_range = exp_time_range(),
       HPs = HPs(),
       frame_rate = frame_rate()
     )}
   )
 }
-
-
-
