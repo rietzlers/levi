e859b27e195139bc00cda9c2718656640114e0a2
Stephan Rietzler <stephan.rietzler@gmx.de>
2020-04-17 13:25:34 +0200

    rename surface_tension_results_vc

--- inst/levi/R/load_dependencies.R
+++ inst/levi/R/load_dependencies.R
@@ -19,14 +19,19 @@ library(seewave)
 # source code which needs to run once -------------------------------------
 source("R/dashboard_vc.R")
 source("R/sample_specs_vc.R")
-source("R/surface_tension_analysis_vc.R")
 source("R/load_analysis_data_vc.R")
-source("R/spectrum_vc.R")
-source("R/surface_tension_results_vc.R")
-source("R/seewaveplots_vc.R")
 source("R/report_notes_vc.R")
-source("R/simulate_data_vc.R")
+
+
+source("R/surface_tension_analysis_vc.R")
 source("R/oscillogramly_vc.R")
+source("R/spectrum_vc.R")
+source("R/surface_tension_plot_vc.R")
+
+source("R/seewaveplots_vc.R")
+
+source("R/simulate_data_vc.R")
+
 # load data ---------------------------------------------------------------
 
 
